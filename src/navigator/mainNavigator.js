60ac6c435ad6106c269ef630bb4804a4ef528db0
Crowdbotics <team@crowdbotics.com>
2020-12-10 15:33:45 +0000

    remove package blueprint-react-native-profiles-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,7 +6,6 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import Settings184068Navigator from '../features/Settings184068/navigator';
-import UserProfile184061Navigator from '../features/UserProfile184061/navigator';
 import Settings184060Navigator from '../features/Settings184060/navigator';
 import Settings184058Navigator from '../features/Settings184058/navigator';
 import SignIn2184056Navigator from '../features/SignIn2184056/navigator';
@@ -19,7 +18,6 @@ const AppNavigator = {
 
     //@BlueprintNavigationInsertion
 Settings184068: { screen: Settings184068Navigator },
-UserProfile184061: { screen: UserProfile184061Navigator },
 Settings184060: { screen: Settings184060Navigator },
 Settings184058: { screen: Settings184058Navigator },
 SignIn2184056: { screen: SignIn2184056Navigator },
